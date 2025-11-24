9a8649171906b55b66d4563d3771b076d8b9dfcd
LitanMolla <litanmolla9@gmail.com>
2025-11-25 00:58:37 +0600

    added: add product form validation

--- src/app/dashboard/add-product/page.js
+++ src/app/dashboard/add-product/page.js
@@ -1,9 +1,15 @@
 'use client'
+import useAuth from '@/hooks/useAuth'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
 const AddProduct = () => {
+    const { user } = useAuth()
     const { register, handleSubmit, formState: { errors } } = useForm()
+    const handleAddProduct = (data) => {
+        const newProduct = { ...data, ownerEmail: user?.email, ownerName: user?.displayName, date: new Date(), stock: 'In stock' }
+        console.log(newProduct)
+    }
     return (
         <>
             <div className="my-10 lg:my-20">
@@ -12,18 +18,19 @@ const AddProduct = () => {
                         <h1 className="heading mb-5 text-center">
                             Add New Product
                         </h1>
-                        <form className="bg-white border border-slate-200 p-6 rounded-xl shadow-sm space-y-5">
+                        <form onSubmit={handleSubmit(handleAddProduct)} className="bg-white border border-slate-200 p-6 rounded-xl shadow-sm space-y-5">
                             {/* Title */}
                             <div>
                                 <label className="block text-sm font-medium text-slate-700 mb-1">
                                     Product Title
                                 </label>
                                 <input
-                                    {...register('title',{required: 'Title is required'})}
+                                    {...register('title', { required: 'Title is required' })}
                                     type="text"
                                     placeholder="Enter product title"
                                     className="w-full border border-slate-300 rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-purple-400 outline-none"
                                 />
+                                {errors?.title && <p className='text-red-500 mt-1 text-sm'>{errors.title.message}</p>}
                             </div>
                             {/* Short Description */}
                             <div>
@@ -31,11 +38,12 @@ const AddProduct = () => {
                                     Short Description
                                 </label>
                                 <input
-                                    {...register('title',{required: 'Short Description is required'})}
+                                    {...register('shortDescription', { required: 'Short Description is required' })}
                                     type="text"
                                     placeholder="1–2 short lines"
                                     className="w-full border border-slate-300 rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-purple-400 outline-none"
                                 />
+                                {errors?.shortDescription && <p className='text-red-500 mt-1 text-sm'>{errors.shortDescription.message}</p>}
                             </div>
 
                             {/* Full Description */}
@@ -44,11 +52,12 @@ const AddProduct = () => {
                                     Full Description
                                 </label>
                                 <textarea
-                                    {...register('title',{required: 'Full Description is required'})}
+                                    {...register('fullDescription', { required: 'Full Description is required' })}
                                     rows={4}
                                     placeholder="Enter detailed product description"
                                     className="w-full border border-slate-300 rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-purple-400 outline-none"
                                 ></textarea>
+                                {errors?.fullDescription && <p className='text-red-500 mt-1 text-sm'>{errors.fullDescription.message}</p>}
                             </div>
 
                             <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
@@ -59,11 +68,12 @@ const AddProduct = () => {
                                         Price ($)
                                     </label>
                                     <input
-                                        {...register('title',{required: 'Price is required'})}
+                                        {...register('price', { required: 'Price is required' })}
                                         type="number"
                                         placeholder="Price"
                                         className="w-full border border-slate-300 rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-purple-400 outline-none"
                                     />
+                                    {errors?.price && <p className='text-red-500 mt-1 text-sm'>{errors.price.message}</p>}
                                 </div>
                                 {/* Priority */}
                                 <div>
@@ -71,7 +81,7 @@ const AddProduct = () => {
                                         Priority
                                     </label>
                                     <select
-                                        {...register('title',{required: 'Price is required'})}
+                                        {...register('priority', { required: 'Priority is required' })}
                                         className="w-full border border-slate-300 rounded-lg px-3 py-2 text-sm bg-white focus:ring-2 focus:ring-purple-400 outline-none"
                                     >
                                         <option value="">Select</option>
@@ -79,6 +89,28 @@ const AddProduct = () => {
                                         <option value="Medium">Medium</option>
                                         <option value="Low">Low</option>
                                     </select>
+                                    {errors?.priority && <p className='text-red-500 mt-1 text-sm'>{errors.priority.message}</p>}
+                                </div>
+                                {/* Category */}
+                                <div>
+                                    <label className="block text-sm font-medium text-slate-700 mb-1">
+                                        Category
+                                    </label>
+                                    <select
+                                        {...register('category', { required: 'Category is required' })}
+                                        className="w-full border border-slate-300 rounded-lg px-3 py-2 text-sm bg-white focus:ring-2 focus:ring-purple-400 outline-none"
+                                    >
+                                        <option value="">Select Category</option>
+                                        <option value="Electronics">Electronics</option>
+                                        <option value="Accessories">Accessories</option>
+                                        <option value="Furniture">Furniture</option>
+                                        <option value="Clothing">Clothing</option>
+                                        <option value="Home Decor">Home Decor</option>
+                                        <option value="Home Decor">Others</option>
+                                    </select>
+                                    {errors?.category && (
+                                        <p className="text-red-500 mt-1 text-sm">{errors.category.message}</p>
+                                    )}
                                 </div>
 
                             </div>
@@ -88,11 +120,12 @@ const AddProduct = () => {
                                     Image URL
                                 </label>
                                 <input
-                                    {...register('title',{required: 'Image URL is required'})}
+                                    {...register('photo', { required: 'Image URL is required' })}
                                     type="url"
                                     placeholder="https://example.com/image.jpg"
                                     className="w-full border border-slate-300 rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-purple-400 outline-none"
                                 />
+                                {errors?.photo && <p className='text-red-500 mt-1 text-sm'>{errors.photo.message}</p>}
                             </div>
                             {/* Submit */}
                             <button
