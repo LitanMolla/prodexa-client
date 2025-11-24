79893570e7e659fbfeb462f57bd9cd229cd4b0ef
LitanMolla <litanmolla9@gmail.com>
2025-11-25 05:06:29 +0600

    added: usr profile on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,14 @@ import React, { useState } from 'react'
 import CustomLink from '../ui/CustomLink'
 import useAuth from '@/hooks/useAuth'
 import { FaBars, FaWindowClose } from "react-icons/fa";
+import Image from 'next/image'
+import { toast } from 'react-toastify'
 
 const Navbar = () => {
     const { user, userLogOut } = useAuth()
     const [isOpen, setIsOpen] = useState(false)
-    // console.log(user)
+    const [infoOpen, setInfoOpen] = useState(false)
+    console.log(user)
     const link = <>
         <li>
             <CustomLink
@@ -59,12 +62,24 @@ const Navbar = () => {
                     <ul className='lg:flex gap-5 flex-wrap hidden'>
                         {link}
                     </ul>
-                    {!user && <Link className='btn-primary hidden! lg:inline-block' href='/login'>Login</Link>}
-                    <button onClick={()=>setIsOpen(!isOpen)} className='text-2xl cursor-pointer lg:hidden'><FaBars /></button>
+                    {!user && <Link className='btn-primary hidden! lg:inline-block!' href='/login'>Login</Link>}
+                    <button onClick={() => setIsOpen(!isOpen)} className='text-2xl cursor-pointer lg:hidden'><FaBars /></button>
+                    {user
+                        &&
+                        <div>
+                            <button onClick={() => setInfoOpen(!infoOpen)} className='btn-primary rounded-full! p-px! relative'>
+                                <img className='size-12 rounded-full' src={user?.photoURL || 'https://img.freepik.com/premium-vector/male-face-avatar-icon-set-flat-design-social-media-profiles_1281173-3806.jpg'} alt={user?.displayName} />
+                                {infoOpen && <div className="absolute top-[66px] right-0 bg-slate-200 text-slate-900 p-5 rounded-md space-y-2">
+                                    <p className='whitespace-nowrap text-lg font-medium'>{user?.displayName}</p>
+                                    <p className='whitespace-nowrap'>{user?.email}</p>
+                                    <button onClick={() => userLogOut().then(() => toast.info('Logged out successfully'))} className='btn-primary w-full'>Logout</button>
+                                </div>}
+                            </button>
+                        </div>}
                 </div>
             </nav>
-            <div className={`bg-slate-900/80 backdrop-blur-2xl fixed min-h-screen top-0 right-0 w-2/3 duration-700 lg:hidden ${isOpen?'translate-x-0':'translate-x-full'}`}>
-                <button onClick={()=>setIsOpen(!isOpen)} className='text-2xl cursor-pointer lg:hidden absolute right-[18px] top-4 text-slate-200'><FaWindowClose /></button>
+            <div className={`bg-slate-900/80 backdrop-blur-2xl fixed min-h-screen top-0 right-0 w-2/3 duration-700 lg:hidden ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+                <button onClick={() => setIsOpen(!isOpen)} className='text-2xl cursor-pointer lg:hidden absolute right-[18px] top-4 text-slate-200'><FaWindowClose /></button>
                 <ul className='text-slate-300 text-center mt-20'>
                     {link}
                 </ul>
--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'http://localhost:5000'
+    // baseURL: 'http://localhost:5000' // local
+    baseURL: 'https://prodexa-api.vercel.app/' // live
 });
 const useAxios = () => {
     return instance
