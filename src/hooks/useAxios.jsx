df191decaba2c22c85c757d3953369cfee889e19
LitanMolla <litanmolla9@gmail.com>
2025-11-25 19:25:48 +0600

    base url

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
     // baseURL: 'http://localhost:5000' // local
-    baseURL: 'https://prodexa-api.vercel.app/' // live
+    baseURL: 'https://prodexa-api.vercel.app' // live
 });
 const useAxios = () => {
     return instance
